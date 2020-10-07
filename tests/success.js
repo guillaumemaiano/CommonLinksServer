5e1888a1ddbb78f0e251c44363a6305d6e98956d
guillaume MAIANO <gem@Executor.local>
2020-10-07 20:47:40 +0200

    A test that runs fine in local to prove out the Jenkins job

---
+++ tests/success.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('PoC', function () {
+  describe('Running any test', function () {
+    it('should always be true', function() {
+      assert.isTrue;		
+      });
+    });
+});
